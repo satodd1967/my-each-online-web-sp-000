11688496eac4fc9519d3d1e7d5128598a47c2c0b
Seth Todd <seth.todd@outlook.com>
2020-01-30 01:41:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,9 @@
 def my_each(array)
-  if block_given?
     i = 0
      while i < array.length
       yield(array[i])
       i = i + 1
     end
      array
-  else
-    puts "Hey! No block was given!"
   end
 end
